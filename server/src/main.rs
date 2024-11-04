3daed0a1492eae12f99d43fe2d29d64d49bf739f
tymek Lewandowski <tymek@tymeks-MacBook-Air.local>
2024-11-04 09:47:49 +0000

    Attempting to setup websockets

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,15 @@
+use axum::extract::ws::{Message, WebSocket};
+use axum::extract::{ConnectInfo, WebSocketUpgrade};
+use axum::response::IntoResponse;
+use axum::routing::any;
 use axum::Router;
+use axum_extra::headers::UserAgent;
+use axum_extra::TypedHeader;
 use log::info;
 use std::env;
+use std::net::SocketAddr;
 use tower_http::cors::CorsLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
@@ -9,7 +17,15 @@ async fn main() {
     if env::var("RUST_LOG").is_err() {
         env::set_var("RUST_LOG", "info")
     }
-    env_logger::init();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let port = match env::var("PORT") {
         Ok(port) => port,
@@ -18,7 +34,7 @@ async fn main() {
 
     let cors = CorsLayer::new();
 
-    let app = Router::new().layer(cors);
+    let app = Router::new().route("/ws", any(ws_handler)).layer(cors);
 
     let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", port))
         .await
@@ -26,4 +42,26 @@ async fn main() {
 
     info!("Listening on : {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    user_agent: Option<TypedHeader<UserAgent>>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+        user_agent.to_string()
+    } else {
+        String::from("Unknown browser")
+    };
+
+    println!("{user_agent} connected at {addr}");
+
+    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+}
+
+async fn handle_socket(mut socket: WebSocket, addr: SocketAddr) {
+    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
+        println!("Pinged {addr}");
+    }
 }
