4063313f0fd3d8f0120a98589d20186c27cb3861
tymek Lewandowski <tymek@tymeks-MacBook-Air.local>
2024-11-06 12:13:37 +0000

    Revered to original method now going to do some work on the frontend

--- server/src/player.rs
+++ server/src/player.rs
@@ -1,45 +1,46 @@
 use crate::game::GameMessage;
 use axum::extract::ws::{Message, WebSocket};
 use futures::{
-    channel::mpsc::Sender,
     stream::{SplitSink, SplitStream},
     StreamExt,
 };
 use log::{error, info};
-use tokio::sync::mpsc::Receiver;
+use tokio::sync::mpsc::{Receiver, Sender};
 use uuid::Uuid;
 
 pub struct Player {
     pub id: Uuid,
-    pub rx: Receiver<GameMessage>,
     pub tx: Sender<GameMessage>,
 }
 
 impl Player {
-    pub fn new(rx: Receiver<GameMessage>, tx: Sender<GameMessage>, socket: WebSocket) -> Self {
+    pub fn new(tx: Sender<GameMessage>, socket: WebSocket) -> Self {
         Self {
             id: Uuid::new_v4(),
-            rx,
             tx,
         }
     }
 
-    pub fn start(&self, socket: WebSocket) {
+    pub fn start(&self, socket: WebSocket, rx: Receiver<GameMessage>) {
         let (sender, reciever) = socket.split();
-        tokio::spawn(async move { Player::writer(sender , self.rx) });
-        tokio::spawn(async move { Player::reader(reciever) });
+        let tx = self.tx.clone();
+        tokio::spawn(async move { Player::writer(sender, rx) });
+        tokio::spawn(async move { Player::reader(reciever, tx) });
     }
 
     // Sends information to the player reciever from game
-    pub async fn writer(sender: SplitSink<WebSocket, Message> , reciever : Receiver<GameMessage>) {
+    pub async fn writer(
+        sender: SplitSink<WebSocket, Message>,
+        mut reciever: Receiver<GameMessage>,
+    ) {
         info!("Writer");
     }
     // Sends information to the game reciever from player
-    pub async fn reader(mut reciever: SplitStream<WebSocket>) {
+    pub async fn reader(mut reciever: SplitStream<WebSocket>, sender: Sender<GameMessage>) {
         while let Some(rcv) = reciever.next().await {
             match rcv {
                 Ok(msg) => match msg {
-                    GameMessage::Text(txt) =>
+                    Message::Text(txt) => {}
                     _ => {}
                 },
                 Err(e) => error!("Something went wrong with socket : {}", e),
